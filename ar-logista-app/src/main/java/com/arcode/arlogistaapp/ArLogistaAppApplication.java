c85d8ef89af91f41d8073c2880bb690c2effb692
Can ARAS <arascan19@outlook.com>
2025-04-19 19:26:15 +0300

    Add files via upload

---
+++ ar-logista-app/src/main/java/com/arcode/arlogistaapp/ArLogistaAppApplication.java
@@ -0,0 +1,13 @@
+package com.arcode.arlogistaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArLogistaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArLogistaAppApplication.class, args);
+    }
+
+}
